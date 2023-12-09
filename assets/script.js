eedf55e397cc456808fb34595637231a02c798ad
VZ13 <113267970+VZachary13@users.noreply.github.com>
2023-12-08 17:50:23 -0700

    Added functions to scripts.js and additional list items to the js section of index.html

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,31 @@
+// variable assignments
 var topics = ['HTML','CSS', 'Git', 'JavaScript'];
+var randomtTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x])
+//list topic function displays entire topics array
+function listTopics() {
+    for (x = 0; x < topics.length; x++) {
+        console.log(topics[x])
+    }
 }
 
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+//select topic function uses Math object randomizer variable to select a random topic
+function selectTopic() {
+    if (randomtTopic === 'HTML') {
+    console.log("Let's study HTML!");
+    } else if (randomtTopic === 'CSS') {
+    console.log("Let's study CSS!");
+    } else if (randomtTopic === 'Git') {
+    console.log("Let's study Git!");
+    } else if (randomtTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+    } else {
+    console.log('Please try again!');
+    } 
 }
-*/
+
+//called functions
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
